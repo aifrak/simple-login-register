9be77330261bba0ccd9301280dd05e0c96d561f8
aifrak <extrem_gamer@hotmail.com>
2016-04-08 15:14:17 +0200

    Change main title page

--- public/common/header.php
+++ public/common/header.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" type="text/css" href="/public/css/main.css">
     <script src="/public/javascript/jquery-2.2.2.js"></script>
     <script src="/public/javascript/common.js"></script>
-    <title>PHP Web</title>
+    <title>Welcome</title>
 </head>
 
 <body>
